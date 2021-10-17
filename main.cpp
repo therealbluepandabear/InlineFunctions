f3340cdb5214cc4d9c08ae11525a18cb9a138d02
xTheCoderx <justaguyoney@gmail.com>
2021-10-17 15:34:53 +1300

    Added 'inline' keyword to 'Square()'' to reduce function call overhead.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 
-//int Square(int x) {
-//    return x * x;
-//}
-
-#define Square(x) x * x
+inline int Square(int x) {
+    return x * x;
+}
 
 int main() {
     int val = 5;
-    int result = Square(val);
+    int result = Square(val + 1);
 
     std::cout << result << std::endl;
     return 0;
